4c0b7b3a77401decf578439d60ce7993f238fa6c
Mathieu <mathieu.delacroix.fr@gmail.com>
2020-11-22 10:02:11 +0100

    repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'params-merge-error-repro';
+
+  constructor(
+    private router:Router,
+  ) {}
+
+  public ngOnInit(): void {
+    this.router.navigate(['/', 'home'], {queryParamsHandling: 'merge'});
+  }
 }
